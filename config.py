068c69808daaed5372dbb9ee5498b1b2c44b3a3e
Wang Meng <104180884+LooKing9218@users.noreply.github.com>
2024-07-01 10:36:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Path with datasets
 PATH_DATASETS = "/raid/wangmeng/Project/IdeaTest/LinT/FoundLIP/DTS/Dataset/"
-# Path with pretraining and transferability dataframes
-PATH_DATAFRAME_PRETRAIN = "/raid/wangmeng/Project/IdeaTest/LinT/FoundLIP/DTS/csv_files/Total_0108_Cleaned_Combined_Public_0109.csv"
+# Path with pretraining dataframes
+PATH_DATAFRAME_PRETRAIN = "/raid/wangmeng/Project/IdeaTest/LinT/FoundLIP/DTS/csv_files/Pretraining.csv"
